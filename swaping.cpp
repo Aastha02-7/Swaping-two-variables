a9b67316466ac50e7aa43e03490b94e826b01d35
Aastha02-7 <53691699+Aastha02-7@users.noreply.github.com>
2019-11-07 21:10:06 +0700

    Add files via upload

---
+++ swaping.cpp
@@ -0,0 +1,14 @@
+//swaping two variable without using third variable
+#include<conio.h>
+#include<iostream>
+using namespace std;
+int main()
+{
+	int x,y;
+	cout<<"Enter a value x= ";
+	cin>>x;
+	cout<<"Enter a value y= ";
+	cin>>y;
+	cout<<"Values has been swaped x= "<<x<<" y= "<<y;
+	return 0;
+}
